8022db0c2702216ce9fe252174d56ee81a32d748
Alyssa <aransbury@gmail.com>
2016-04-07 11:27:54 -0400

    Add review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ActiveRecord::Base
+  validates :review_text, presence: true
+
+  validate :unique_review
+
+  def unique_review
+    # Check to make sure user has not already reviewed that toilet.
+  end
+
 end
