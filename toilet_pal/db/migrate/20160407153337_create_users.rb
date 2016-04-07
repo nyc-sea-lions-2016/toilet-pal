a13ac3f354358e820d0a6d5107e32f57a0fc8240
John Maguire <jmaguire5588@gmail.com>
2016-04-07 17:18:43 -0400

    commit before merge

--- toilet_pal/db/migrate/20160407153337_create_users.rb
+++ toilet_pal/db/migrate/20160407153337_create_users.rb
@@ -1,4 +1,4 @@
-class CreateUsers < ActiveRecord::Migration
+fgclass CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :username, null: false, index: true, unique:true
