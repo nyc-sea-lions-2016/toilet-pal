a36f4e00228fc3e49f0b2c1b37de3227a331c021
FJunior225 <fcollins@live.com>
2016-04-07 12:10:05 -0400

    Include gems for bcrypt and avatar - create user model and migration with validations

--- toilet_pal/Gemfile
+++ toilet_pal/Gemfile
@@ -22,7 +22,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+# avatars for users: avatars_for_rails:install user
+gem 'avatars_for_rails'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
---
+++ toilet_pal/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  acts_as_avatarable
+
+  # has_many :favorites
+  # has_many :favorite_toilets, through: :favorites, class_name: "Toilet", foriegn_key: "user_id"
+  # has_many :reviews
+  # has_many :reviewed_toilets, through: :reviews, class_name: ""
+
+  validates :username, {presence: true, uniqueness: true}
+  validates :email, :first_name, :last_name, :zip_code, :gender, {presence: true}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+end
