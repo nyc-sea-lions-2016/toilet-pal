e938c9bb3e1f01933fe9d4a43e4193b7b289afa6
FJunior225 <fcollins@live.com>
2016-04-07 16:39:08 -0400

    Add user authentication with nav bar options

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   get 'toilets/index'
-  get 'users/new'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
