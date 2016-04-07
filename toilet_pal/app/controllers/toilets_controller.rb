fd6bd2895bbe84e4850feae4d1c56c085c8d34a6
John Maguire <jmaguire5588@gmail.com>
2016-04-07 16:26:09 -0400

    Start toilet show page

--- toilet_pal/app/controllers/toilets_controller.rb
+++ toilet_pal/app/controllers/toilets_controller.rb
@@ -2,5 +2,9 @@ class ToiletsController < ApplicationController
   def index
   end
 
+  def show
+  	@toilet = Toilet.find_by(id: params[:id])
+  	@average_review = Toilet.average_review(@toilet)
+  end
 
 end
