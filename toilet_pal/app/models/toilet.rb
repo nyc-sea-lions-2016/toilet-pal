72dd2397e99502336f5a6ad430853068d3b3f6e1
John Maguire <jmaguire5588@gmail.com>
2016-04-07 16:11:03 -0400

    Fixed associations for reviews, users, toilets

--- toilet_pal/app/models/toilet.rb
+++ toilet_pal/app/models/toilet.rb
@@ -1,6 +1,8 @@
 class Toilet < ActiveRecord::Base
 	has_many :reviews
 	has_many :favorites
+	has_many :reviewers, through: :reviews, class_name: 'User', foreign_key: 'reviewer_id'
+	has_many :favoriters, through: :favorites
 
 	validates :name, :location, :description, presence: true
 
--- toilet_pal/config/routes.rb
+++ toilet_pal/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'user/new'
   get 'users/new'
-
+  
   resources :toilets
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
