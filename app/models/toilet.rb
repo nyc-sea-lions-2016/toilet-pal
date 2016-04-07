4bf298d80bc863244cc16734acee57e44ae05b92
John Maguire <jmaguire5588@gmail.com>
2016-04-07 16:26:09 -0400

    Start toilet show page

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -6,4 +6,12 @@ class Toilet < ActiveRecord::Base
 
 	validates :name, :location, :description, presence: true
 
+	def self.average_review(toilet)
+		if (toilet.reviews.length) > 0
+			(toilet.reviews.reduce(0){|sum,review| sum += review})/(toilet.reviews.length)
+		else 
+			'Be the first to review this toilet!'
+		end
+	end
+
 end
