c9ebf2e9f618d7899486d94698f8f4ede6360880
Alyssa <aransbury@gmail.com>
2016-04-07 18:48:34 -0400

    Updated migration for toilets to include zip code, neighborhood, sublocality, latitude and longitude

--- toilet_pal/db/seeds.rb
+++ toilet_pal/db/seeds.rb
@@ -29,12 +29,13 @@ end
 end
 
 Toilet.all.each do |toilet|
-	5.times do
-		Review.create({
-			toilet_id: toilet.id,
-			review_text: Faker::Hipster.paragraphs(3)[0] + Faker::Hipster.paragraphs(3)[1] + Faker::Hipster.paragraphs(3)[2],
-			reviewer_id: rand(1..20),
-			rating: rand(1..5)
-			})
-	end
+    address = toilet.location.gsub(" ", "+")
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyALN7GVrxfs8xQmQ1Rn1AXZe-uOGd3muVU"
+  response = HTTParty.get(url)
+    toilet.zip_code = nil
+    toilet.neighborhood = response["results"][0]["address_components"][1]["long_name"] || nil
+    toilet.sublocality = response["results"][0]["address_components"][2]["long_name"] || nil
+    toilet.latitude = response["results"][0]["geometry"]["location"]["lat"] || nil
+    toilet.longitude = response["results"][0]["geometry"]["location"]["lng"] || nil
+    toilet.save
 end
