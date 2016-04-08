8c5549afa9a2a0f493c0571eb73236ac4533cf80
John Maguire <jmaguire5588@gmail.com>
2016-04-07 22:34:14 -0400

    Finish adding reviews to toilet show page, needs error handling still

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
-
+  resources :reviews
   resources :toilets
-
+  get '/toilets/:id/review' => 'reviews#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
