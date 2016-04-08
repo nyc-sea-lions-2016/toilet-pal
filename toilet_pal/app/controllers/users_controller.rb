1077b2949f54525ac7a339b968d4673c252484f0
FJunior225 <fcollins@live.com>
2016-04-08 15:01:59 -0400

    save point

--- toilet_pal/app/controllers/users_controller.rb
+++ toilet_pal/app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
