7335cc47715d8626f34ec6a185c7366b24cedd23
Alyssa <aransbury@gmail.com>
2016-04-10 17:23:45 -0400

    Problems with search box not returning homepage

--- toilet_pal/app/controllers/toilets_controller.rb
+++ toilet_pal/app/controllers/toilets_controller.rb
@@ -2,11 +2,9 @@ class ToiletsController < ApplicationController
   def index
     if params.has_key?(:user) && params[:user].has_key?(:user_input)
       geo_info = Geocoder.search(params[:user][:user_input])
-      binding.pry
         user_search_lat = geo_info[0].data["geometry"]["location"]["lat"]
         user_search_lon = geo_info[0].data["geometry"]["location"]["lng"]
         @search_location = [user_search_lat,user_search_lon]
-    binding.pry
         #need to throw error if geo_info could not parse answer correctly.
     else
       @search_location = [40.7831, -73.9712] #center manhattan
@@ -16,7 +14,6 @@ class ToiletsController < ApplicationController
     @all_toilets = Toilet.all.select do |toilet|
       toilet.distance_to(@search_location[0], @search_location[1]) < 1609
     end
-
     @toilets = @all_toilets.sort{|x,y| x.distance_to(@search_location[0], @search_location[1]) <=> y.distance_to(@search_location[0], @search_location[1])}
   end
 
@@ -32,7 +29,7 @@ class ToiletsController < ApplicationController
   end
 
   def user_search
-    redirect_to index
+    redirect_to index and return
   end
 
 end
