3cf4553af1eea92da4bbf3859dc84d213afb732d
Alyssa <aransbury@gmail.com>
2016-04-10 17:01:18 -0400

    Add functioning search box on homepage

--- toilet_pal/app/controllers/toilets_controller.rb
+++ toilet_pal/app/controllers/toilets_controller.rb
@@ -1,13 +1,23 @@
 class ToiletsController < ApplicationController
   def index
+    if params.has_key?(:user) && params[:user].has_key?(:user_input)
+      geo_info = Geocoder.search(params[:user][:user_input])
+      binding.pry
+        user_search_lat = geo_info[0].data["geometry"]["location"]["lat"]
+        user_search_lon = geo_info[0].data["geometry"]["location"]["lng"]
+        @search_location = [user_search_lat,user_search_lon]
     binding.pry
-    @center_manhattan = [40.7831, -73.9712]
-
-    # toilets within ten miles (in meters) of central manhattan:
-    @all_toilets = Toilet.all.select do |toilet|
-      toilet.distance_to(@center_manhattan[0], @center_manhattan[1]) < 16093
+        #need to throw error if geo_info could not parse answer correctly.
+    else
+      @search_location = [40.7831, -73.9712] #center manhattan
     end
-    @toilets = @all_toilets.sort{|x,y| x.distance_to(@center_manhattan[0], @center_manhattan[1]) <=> y.distance_to(@center_manhattan[0], @center_manhattan[1])}
+
+    # toilets within one mile (in meters) of central manhattan or user search input:
+    @all_toilets = Toilet.all.select do |toilet|
+      toilet.distance_to(@search_location[0], @search_location[1]) < 1609
+    end
+
+    @toilets = @all_toilets.sort{|x,y| x.distance_to(@search_location[0], @search_location[1]) <=> y.distance_to(@search_location[0], @search_location[1])}
   end
 
 
@@ -22,9 +32,7 @@ class ToiletsController < ApplicationController
   end
 
   def user_search
-    binding.pry
-    @user_input = params[:user][:user_input]
-    render :index
+    redirect_to index
   end
 
 end
