36d7f4439200008a981787dee1d57a236503e0c5
FJ Collins Jr <fcollins@live.com>
2016-05-19 16:35:07 -0400

    update google omniauth to match oauth2

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -9,8 +9,14 @@ class CallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user
     end
 
-    def google
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
+    def google_oauth2
+        @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+        if @user.persisted?
+          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+          sign_in_and_redirect @user, :event => :authentication
+        else
+          session["devise.google_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
     end
 end
