623419214a81e9765ce5a1d5ee7f66f88a5cb0eb
FJ Collins Jr <fcollins@live.com>
2016-05-24 13:24:40 -0400

    add identities model and migration to inherit from User and move find_for_oauth method to identity model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519170700) do
+ActiveRecord::Schema.define(version: 20160524153034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,24 @@ ActiveRecord::Schema.define(version: 20160519170700) do
 
   add_index "favorites", ["favoriter_id"], name: "index_favorites_on_favoriter_id", using: :btree
   add_index "favorites", ["toilet_id"], name: "index_favorites_on_toilet_id", using: :btree
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "accesstoken"
+    t.string   "refreshtoken"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "email"
+    t.string   "nickname"
+    t.string   "image"
+    t.string   "phone"
+    t.string   "urls"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "reviewer_id", null: false
@@ -83,4 +101,5 @@ ActiveRecord::Schema.define(version: 20160519170700) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "identities", "users"
 end
